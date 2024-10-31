d502399dd2660b039ae886b16b86cedabd7f4eed
Adam <silhavik.adam@gmail.com>
2024-10-31 21:59:28 +0100

    Add mobile menu functionality with toggle for navigation and buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+function waitForPageLoad(callback) {
+    if (document.readyState === 'complete') {
+        callback();
+    } else {
+        window.addEventListener('load', callback);
+    }
+}
+
+waitForPageLoad(function() {
+    var menu_button = document.getElementById('menu_button');
+    menu_button.addEventListener('click', function() {
+        var header_nav = document.getElementById('header_nav');
+        var header_buttons = document.getElementById('header_buttons');
+        header_nav.classList.toggle('open');
+        header_buttons.classList.toggle('open');
+    });
+});
