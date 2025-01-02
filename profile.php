d1abf2153ce23e08971fda157f0f771b8fe8e6e4
Adam <silhavik.adam@gmail.com>
2025-01-02 16:05:52 +0100

    Remove session check for username in profile page

--- profile.php
+++ profile.php
@@ -1,11 +1,6 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit();
-}
-
 $username = $_SESSION['username'];
 $role = $_SESSION['role'];
 $userPhoto = "images/default-profile.jpg";
